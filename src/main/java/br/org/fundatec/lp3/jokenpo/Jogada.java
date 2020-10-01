ac124fef0b183bc9831229f4f65fbd4341afb93d
Vinicius <viniciusvks@gmail.com>
2020-10-01 10:53:58 -0300

    Refatoracao da classe Jogo

---
+++ src/main/java/br/org/fundatec/lp3/jokenpo/Jogada.java
@@ -0,0 +1,33 @@
+package br.org.fundatec.lp3.jokenpo;
+
+public enum Jogada {
+
+	PEDRA(0),
+	PAPEL(1),
+	TESOURA(2);
+	
+	private int valor;
+	
+	Jogada(int valor) {
+		this.valor = valor;
+	}
+	
+	public int getValor() {
+		return this.valor;
+	}
+	
+	public static Jogada comValor(int valor) {
+		
+		Jogada[] jogadas = values();
+		
+		for(Jogada jogada : jogadas) {
+			if(jogada.getValor() == valor) {
+				return jogada;
+			}
+		}
+		
+		return null;
+		
+	}
+	
+}
--- src/test/java/br/org/fundatec/lp3/jokenpo/JogoTest.java
+++ src/test/java/br/org/fundatec/lp3/jokenpo/JogoTest.java
@@ -41,7 +41,7 @@ public class JogoTest {
 	}
 	
 	@Test
-	public void jogoDeveRetornarPedraVenceParaPedraContraPapel() {
+	public void jogoDeveRetornarPapelVenceParaPedraContraPapel() {
 		
 		//Cenario
 		Jogo jogo = new Jogo();
@@ -57,6 +57,89 @@ public class JogoTest {
      	
 	}
 	
+	@Test
+	public void jogoDeveRetornarPedraVenceParaPedraContraTesoura() {
+		
+		//Cenario
+		Jogo jogo = new Jogo();
+		int jogada1 = 0; //Pedra
+		int jogada2 = 2; //Tesoura
+     	
+		//Execucao
+		String resultado = jogo.executar(jogada1, jogada2);
+				
+		//Validacao
+		String resultadoEsperado = "Pedra venceu!";
+     	assertEquals(resultadoEsperado, resultado);
+     	
+	}
 	
+	@Test
+	public void jogoDeveRetornarPapelVenceParaPapelContraPedra() {
+		
+		//Cenario
+		Jogo jogo = new Jogo();
+		int jogada1 = 1; //papel
+		int jogada2 = 0; //Pedra
+     	
+		//Execucao
+		String resultado = jogo.executar(jogada1, jogada2);
+				
+		//Validacao
+		String resultadoEsperado = "Papel venceu!";
+     	assertEquals(resultadoEsperado, resultado);
+     	
+	}
+	
+	@Test
+	public void jogoDeveRetornarTesouraVenceParaPapelContraTesoura() {
+		
+		//Cenario
+		Jogo jogo = new Jogo();
+		int jogada1 = 1; //Papel
+		int jogada2 = 2; //Tesoura
+     	
+		//Execucao
+		String resultado = jogo.executar(jogada1, jogada2);
+				
+		//Validacao
+		String resultadoEsperado = "Tesoura venceu!";
+     	assertEquals(resultadoEsperado, resultado);
+     	
+	}
+
+	@Test
+	public void jogoDeveRetornarPedraVenceParaTesouraContraPedra() {
+		
+		//Cenario
+		Jogo jogo = new Jogo();
+		int jogada1 = 2; //Tesoura
+		int jogada2 = 0; //Pedra
+     	
+		//Execucao
+		String resultado = jogo.executar(jogada1, jogada2);
+				
+		//Validacao
+		String resultadoEsperado = "Pedra venceu!";
+     	assertEquals(resultadoEsperado, resultado);
+     	
+	}
+	
+	@Test
+	public void jogoDeveRetornarTesouraVenceParaTesouraContraPapel() {
+		
+		//Cenario
+		Jogo jogo = new Jogo();
+		int jogada1 = 2; //Tesoura
+		int jogada2 = 1; //Papel
+     	
+		//Execucao
+		String resultado = jogo.executar(jogada1, jogada2);
+				
+		//Validacao
+		String resultadoEsperado = "Tesoura venceu!";
+     	assertEquals(resultadoEsperado, resultado);
+     	
+	}
 
 }
